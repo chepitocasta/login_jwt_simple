17c60fe4abfa95a436d77fa943e14674955480e3
José Castañeda <josecasta.r@gmail.com>
2019-11-13 23:51:24 -0600

    Se agrego el paquete dotenv y corr-env para manejar variables de entorno y ambiente de desarrollo

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+  console.log("Bienvenido al ambiente: ", process.env.NODE_ENV);
+}
+
 const express = require("express");
 const app = express();
 
